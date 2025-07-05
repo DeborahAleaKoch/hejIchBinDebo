9259cd0aef903a2b114f1008dc07c2f0a4fba09f
DeborahAleaKoch <alea-de@web.de>
2025-07-05 19:09:34 +0200

    typo

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -5,7 +5,7 @@ export const Quiz = () => {
 	return (
 		<>
 			<section>
-				<h1 className=' text-3xl text-center mt-2 font-semibold italic dark:text-(--color-secondary) text-(--color-secondary)'>
+				<h1 className='text-3xl text-center mt-2 font-semibold italic dark:text-(--color-secondary) text-(--color-secondary)'>
 					Yay,{" "}
 					<span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-(--color-primary)'>
 						<span className='relative text-white dark:text-gray-950'>
