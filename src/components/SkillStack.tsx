39a88954f2ad5f4d566f3dc12b4cf5a5becf87b6
DeborahAleaKoch <alea-de@web.de>
2025-07-08 11:17:26 +0200

    little hover effect at the skill logos

--- src/components/SkillStack.tsx
+++ src/components/SkillStack.tsx
@@ -11,7 +11,7 @@ export const SkillStack: React.FunctionComponent<Props> = ({ skill }) => {
 			data-tip={skill.tooltip}
 		>
 			{skill.text}
-			<img src={`${skill.logo}`} alt={skill.text} className='w-21 ' />
+			<img src={`${skill.logo}`} alt={skill.text} className='w-21 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-120' />
 		</div>
 	);
 };
