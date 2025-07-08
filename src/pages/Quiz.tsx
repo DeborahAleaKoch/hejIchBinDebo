663a82a31245a013bd1a304bef137bbc0df3116b
DeborahAleaKoch <alea-de@web.de>
2025-07-08 11:35:40 +0200

    add component

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,3 +1,4 @@
+import { Headline } from "../components/Headline";
 import { QuizBox } from "../components/QuizBox";
 import { quizQuestions } from "../quizData";
 
@@ -5,14 +6,8 @@ export const Quiz = () => {
 	return (
 		<>
 			<section>
-				<h1 className='text-3xl text-center mt-2 font-semibold italic dark:text-(--color-secondary) text-(--color-secondary)'>
-					Yay,{" "}
-					<span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-(--color-primary)'>
-						<span className='relative text-white dark:text-gray-950'>
-							Quiz Time!
-						</span>
-					</span>
-				</h1>
+				<Headline title=' Yay, Quiz Time!' />
+
 				{quizQuestions.map((item) => (
 					<QuizBox item={item} />
 				))}
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,14 +1,11 @@
+import { Headline } from "../components/Headline";
 import { TechSkill } from "../components/TechSkill";
 import { goodSkills, techStackData, wantToLearn } from "../data";
 
 export const Skills = () => {
 	return (
 		<div className='mx-48 text-center'>
-			<div className='text-3xl mt-2 font-semibold italic dark:text-(--color-secondary) text-(--color-secondary) py-3'>
-				<span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-(--color-primary)'>
-					<h1 className='relative text-white dark:text-gray-950'>Tech Stack</h1>
-				</span>
-			</div>
+			<Headline title='Tech Stack' />
 
 			<section className=' text-left '>
 				<article className=''>
