e363c6a295ded0f06c1688dab3812448ed3c54f0
DeborahAleaKoch <alea-de@web.de>
2025-07-08 16:27:56 +0200

    add text about me - wip

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,8 +1,40 @@
+import { Headline } from "../components/Headline";
+
 export const AboutMe = () => {
 	return (
 		<>
-			<div className=''>
-				<p>hier ist was über mich</p>
+			<Headline title='about me' />
+			<div className='mx-48 my-5'>
+				<section className='flex justify-between gap-10 items-center'>
+					<img
+						src='../../public/img/debo.png'
+						alt='Bild von mir.'
+						className=' rounded-full  bg-gray-50 '
+					/>
+					<article className='text-lg w-2/3 text-justify '>
+						<div className=''>
+							Ich heiße Deborah Alea Koch und bin 1990 in Lünen geboren und
+							aufgewachsen. Von den meisten werde ich Debo genannt und bin damit
+							voll fein.{" "}
+							{/* Der Name <span>Deborah</span> kommt aus dem hebräischen und beudeutet <span>die fleißige Biene</span>.  */}
+							Inzwischen wohne ich in Bochum und habe zwei Kater. In meiner
+							Freizeit mache ich Sport,
+							{/* lese oder koche was leckeres. */}{" "}
+							<div>
+								<br />
+								<h3 className='underline'>fun-facts:</h3>
+								<li>
+									Meine Tassen passen leider nicht alle in einen Schrank, dafür
+									gibt es einen zweiten.{" "}
+								</li>
+								<li>
+									Der Name Deborah stammt aus dem hebräischen und bedeutet so
+									viel wie fleißige Biene 🐝
+								</li>
+							</div>
+						</div>
+					</article>
+				</section>
 			</div>
 		</>
 	);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,50 @@
-export const Home =()=>{
-    return (
-        <div className='px-12'>
-			<h1>Hej ich bin Debo!</h1>
-			<section className='flex justify-around'>
-				<p>hier steht ganz viel toller Text.</p>
+import { Headline } from "../components/Headline";
+
+export const Home = () => {
+	console.log(
+		"Hallo! Schön dass du vorbei schaust. Hier sollte alles aufgeräumt sein, ich habe nämlich mit Besuch gerechnet. Möchtest du einen Kaffee?? ;-)"
+	);
+
+	return (
+		<div className='px-12 mx-36'>
+			<Headline title='Hej! Ich bin Debo!' />
+			<section className='flex justify-around py-8 gap-10'>
+				<article className=' rounded w-1/2 text-lg font-light text-justify '>
+					<p className=''>
+						Ich bin{" "}
+						<span className='italic text-xl'>Frondend Web Developerin. </span>
+					</p>
+					<br />
+					<div className=''>
+						Seit 2024 befinde ich mich in meiner beruflichen Umorientierung. Im
+						Juni 2024 absolvierte ich einen vier wöchigen JavaScript Kurs und im Mai 2025
+						beendete ich ein Bootcamp - mit den Schwerpunkten{" "}
+						{/* <span className='relative inline-block before:absolute before:-inset-0.5 before:block before:-skew-y-3 before:bg-orange-300'>
+							<span className='relative font-medium text-slate-700 dark:text-gray-950'> */}
+						<span className='font-bold italic'>React</span>{" "}
+						{/* </span>
+						</span>{" "} */}
+						und{" "}
+						{/* <span className='relative inline-block before:absolute before:-inset-0.5 before:block before:-skew-y-3 before:bg-orange-300'>
+							<span className='relative font-medium text-slate-700 dark:text-gray-950'> */}
+						<span className='font-bold italic'>TypeScript</span>
+						{/* </span>
+						</span>{" "} */}
+						. Dort lernte ich unter anderem den Umgang mit supabase. Wobei mich
+						das Anlegen eines Backends ohne{" "}
+						<span className='underline italic'>Backend as a Service</span> mehr
+						interessiert, weswegen ich auch gerne{" "}
+						<span className='font-bold italic'>PHP</span> lernen möchte.
+					</div>
+					<br />
+					<p></p>
+					<p>
+						Ich bin ausgebildete Gesundheits- und Kinderkrankenpflegerin und
+						habe zuletzt im Evangelischen Krankenhaus in Hattingen in der
+						neurophysiologischen Abteilung gearbeitet.{" "}
+					</p>
+				</article>
+
 				<img
 					src='../../public/img/27640991-925C-43F5-856A-AF16E5AE9F38_1_105_c.jpeg'
 					alt='Bild von Debo.'
@@ -11,5 +52,5 @@ export const Home =()=>{
 				/>
 			</section>
 		</div>
-    )
-}
+	);
+};
