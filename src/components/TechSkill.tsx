2863d90a97f2ac5de295e92e02aa43a92291ae6c
DeborahAleaKoch <alea-de@web.de>
2025-07-08 10:40:07 +0200

    add color  in index.css

--- src/components/TechSkill.tsx
+++ src/components/TechSkill.tsx
@@ -9,10 +9,10 @@ interface Props {
 export const TechSkill: React.FC<Props> = ({ skills, text }) => {
 	return (
 		<div className={`border-b-1 border-(--color-primary) `}>
-			<h2 className='text-2xl my-4 italic font-extralight border-(--color-primary) border-b-2 inline-block '>
+			<h2 className='text-2xl my-4 italic font-extralight border-(--color-primary) border-b-2  inline-block '>
 				{text}
 			</h2>
-			<p className={`flex flex-wrap gap-14 py-6 `} >
+			<p className={`flex flex-wrap gap-14 py-6 `}>
 				{skills.map((skill) => (
 					<SkillStack skill={skill} />
 				))}
